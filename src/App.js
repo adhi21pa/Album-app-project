56a3bda3248cb693b2c5aabe7edca089c71502be
adhi21pa <adithyapa21@gmail.com>
2024-06-08 20:41:57 +0530

    complete project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import AddAlbum from './components/AddAlbum';
+import SearchAlbum from './components/SearchAlbum';
+import DeleteAlbum from './components/DeleteAlbum';
+import ViewAlbum from './components/ViewAlbum';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div >
+      
+     <BrowserRouter>
+     <Routes>
+      <Route>
+        <Route path='/'element={<AddAlbum/>}/>
+        <Route path='/search' element={<SearchAlbum/>}/>
+        <Route path='/delete' element={<DeleteAlbum/>}/>
+        <Route path='/viewalbum' element={<ViewAlbum/>}/>
+
+        </Route>
+        </Routes>
+        </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
